825aea26b2c05488ffebba5f7d07a83662bc8131
Simon Charette <charette.s@gmail.com>
2024-12-17 23:38:23 -0500

    Refs #35967 -- Deprecated BaseDatabaseCreation.create_test_db(serialize).
    Given there are no longer any internal usages of serialize=True and it poses
    a risk to non-test databases integrity it seems appropriate to deprecate it.

--- tests/test_runner/tests.py
+++ tests/test_runner/tests.py
@@ -946,7 +946,7 @@ class SetupDatabasesTests(unittest.TestCase):
         ):
             self.runner_instance.setup_databases()
         mocked_db_creation.return_value.create_test_db.assert_called_once_with(
-            verbosity=0, autoclobber=False, serialize=False, keepdb=False
+            verbosity=0, autoclobber=False, keepdb=False
         )
         mocked_db_creation.return_value.serialize_db_to_string.assert_called_once_with()
 
